0d23532307ae1007a5b71f41ab41a422fa19b0f1
Joan Rosario <g00283924@stu.raritanval.edu>
2021-10-15 12:33:40 -0400

    UI implemented
    User can now escape into UI, and press button to reload game.

---
+++ Assets/Escapekey1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Escapekey1 : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Scene_0");
+        }
+    }
+}
